ec4dc04685d4c99f6218ef6ecaddfe3a6a990056
Angel Chen <angelchen396@gmail.com>
2024-01-18 16:43:28 -0500

    finished all functions

--- recursion.py
+++ recursion.py
@@ -13,14 +13,18 @@ of the software.
 
 def length(lst: list) -> int:
     """Return the number of elements (top level) in lst."""
-
-    pass
+    if lst == []:
+        return 0
+    lst = lst[1:]
+    return 1 + length(lst)
 
 
 def reverse(lst: list) -> list:
     """Return the reverse of lst."""
-
-    pass
+    if length(lst) <= 1:
+        print(lst)
+        return lst
+    return lst[-1:] + reverse(lst[1:-1]) + lst[:1]
 
 
 def is_pal(lst: list) -> bool:
@@ -28,16 +32,18 @@ def is_pal(lst: list) -> bool:
 
     For fun, do not use an if-statement.
     """
-
-    pass
+    return length(lst) <= 1 or (lst[0] == lst[-1] and is_pal(lst[1:-1]))
 
 
 def num_el(lst: list) -> int:
     """Return the number of (non-list) elements of lst, on any nesting
     level.
     """
-
-    pass
+    if lst == []:
+        return 0
+    if isinstance(lst[0], list):
+        return num_el(lst[0]) + num_el(lst[1:])
+    return 1 + num_el(lst[1:])
 
 
 def sum_els(lst: list) -> int:
@@ -45,8 +51,11 @@ def sum_els(lst: list) -> int:
     0 on an empty list.
 
     """
-
-    pass
+    if lst == []:
+        return 0
+    if isinstance(lst[0], list):
+        return sum_els(lst[0]) + sum_els(lst[1:])
+    return lst[0] + sum_els(lst[1:])
 
 
 def repeat_twice(lst: list) -> list:
@@ -56,8 +65,11 @@ def repeat_twice(lst: list) -> list:
     >>> repeat_twice(['c', '2', '4'])
     ['c', 'c', '2', '2', '4', '4']
     """
-
-    pass
+    if lst == []:
+        return lst
+    double = lst[:1]
+    double.append(lst[0])
+    return double + repeat_twice(lst[1:])
 
 
 def my_filter(func: callable, lst: list) -> list:
@@ -68,8 +80,11 @@ def my_filter(func: callable, lst: list) -> list:
     func is a boolean-valued function applicable to every element of
     lst.
     """
-
-    pass
+    if lst == []:
+        return lst
+    if func(lst[0]):
+        return lst[:1] + my_filter(func, lst[1:])
+    return my_filter(func, lst[1:])
 
 
 def my_map(func: callable, lst: list) -> list:
@@ -77,8 +92,10 @@ def my_map(func: callable, lst: list) -> list:
 
     func is applicable to every element of lst.
     """
-
-    pass
+    if lst == []:
+        return lst
+    mod_list = [func(lst[0])]
+    return mod_list + my_map(func, lst[1:])
 
 
 if __name__ == "__main__":
